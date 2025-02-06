110907bf5d2690872333bf98143ecb8ea9e770ef
Andres Escala <aescalaa1900@alumno.ipn.mx>
2025-02-05 22:03:32 -0600

    Update receptor.ino
    refactor: change variable names, add comments, change libraries, delete unused functions and variables

--- receptor/receptor/receptor.ino
+++ receptor/receptor/receptor.ino
@@ -1,190 +1,197 @@
-#include <SoftwareSerial.h> // Librería para comunicación serial
-#include <ESP8266WiFi.h> // Librería para conexión WiFi
-#include <ESP8266HTTPClient.h> // Librería para hacer peticiones HTTP
-SoftwareSerial sim(D3,D2);
+/* Declaracion de librerias */
+#include <HardwareSerial.h> // Librería para comunicación serial
+#include <WiFi.h> // Librería para conexión WiFi
+#include <HTTPClient.h> // Librería para hacer peticiones HTTP
+
+// Definir el puerto serial SIM800L
+HardwareSerial SIM800L(1);
+
 // Datos de la red WiFi
-const char* ssid = "WIFI";
-const char* password = "PASSWORD";
-// Pin para el LED (indicador de mensaje)
-const int ledPin = D8;
-const int pushB = D6;
-const int pushS = D7;
-const int pushR = D4;
+const char* SSID = "IZZI-53E3";
+const char* PASSWORD = "PASSWORD";
+
+/* Declaracion de puertos del ESP */
+const int LED = 8;
+const int READ_BTN = 4;
+
 // Servidor al cual se hará la petición POST
-const String server = "http://192.168.0.5:5322";
-const String url = server + "/api/tracker/post-test";
+const String SERVER = "http://192.168.0.5:5322";
+const String URL = SERVER + "/api/tracker/post-test";
+
 int _timeout;
 String _buffer;
-const String number = "+525545464585";
-//const String number = "+525554743913";
+
 // Objeto WiFiClient para manejar la conexión
 WiFiClient client;
 void setup() {
-Serial.begin(9600); // Serial para debug
-sim.begin(9600);
-// Configurar el pin del LED como salida
-pinMode(pushB, INPUT);
-pinMode(pushS, INPUT);
-pinMode(pushR, INPUT);
-pinMode(ledPin, OUTPUT);
-digitalWrite(ledPin, HIGH);
-// Conectar a la red WiFi
-Serial.println("Conectando a WiFi...");
-WiFi.begin(ssid, password);
-while (WiFi.status() != WL_CONNECTED) {
-delay(1000);
-Serial.println("Conectando...");
-}
-Serial.println("Conectado a WiFi.");
-Serial.println("----------------");
-// Configuración del SIM800L
-sim.println("AT");
-delay(100);
-sim.println("AT+CMGF=1"); // Modo texto
-delay(100);
-// Leer el mensaje
-sim.println("AT+CMGR=1"); // Leer el primer mensaje
-delay(1000); // Esperar respuesta
-borrarTodosMensajes();
-digitalWrite(ledPin, LOW);
+  Serial.begin(9600); // Serial para debug
+  SIM800L.begin(9600, SERIAL_8N1, 17, 16);
+  // Configurar el pin del LED como salida
+  pinMode(READ_BTN, INPUT);
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, HIGH);
+  // Conectar a la red WiFi
+  Serial.println("Conectando a WiFi...");
+  WiFi.begin(SSID, PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Conectando...");
+  }
+  Serial.println("Conectado a WiFi.");
+  Serial.println("----------------");
+  // Configuración del SIM800L
+  SIM800L.println("AT");
+  delay(100);
+  SIM800L.println("AT+CMGF=1"); // Modo texto
+  delay(100);
+  // Leer el mensaje
+  SIM800L.println("AT+CMGR=1"); // Leer el primer mensaje
+  delay(1000); // Esperar respuesta
+  borrarTodosMensajes();
+  digitalWrite(LED, LOW);
 }
 void loop() {
-if (digitalRead(pushB) == 1) {
-enviarMensaje("Enviando informacion de los rastreadores al servidor");
+  if (digitalRead(READ_BTN) == 1) {
+    Serial.println("lEYENDO mensajeS SMS a prueba");
+    RecieveMessage();
+  }
+  if (Serial.available() > 0) {
+    String message = Serial.readString();
+    Serial.println("Ahol: " + message);
+    if(message == "r"){
+      RecieveMessage();
+    }else{
+      enviarMensaje(Serial.readString());
+    }
+  }
+  if (SIM800L.available()) {
+    Serial.println("Llegó algo");
+    //String message = SIM800L.readString();
+    String message = "";
+    char incomingChar;
+    // Bucle para leer todos los caracteres disponibles
+    while (SIM800L.available()) {
+      incomingChar = SIM800L.read(); // Leer carácter por carácter
+      message += incomingChar; // Agregar carácter al mensaje
+      delay(10); // Pequeña pausa para asegurar la lectura completa
+    }
+    message = cleanString(message);
+    Serial.println("Leido: " + message);
+    int index = validarFormatoCMTI(message);
+    leerMensaje(index);
+  }
 }
-if (digitalRead(pushR) == 1) {
-Serial.println("lEYENDO mensajeS SMS a prueba");
-RecieveMessage();
+
+String cleanString(String message) {
+  message.trim();
+  message.replace("\n","");
+  message.replace("\r","");
+  message.replace("\"", "'");
+  return message;
 }
-if (Serial.available() > 0) {
-String message = Serial.readString();
-Serial.println("Ahol: " + message);
-if(message == "r"){
-RecieveMessage();
-}else{
-  enviarMensaje(Serial.readString());
-}
-}
-if (sim.available()) {
-Serial.println("Llegó algo");
-//String message = sim.readString();
-String message = "";
-char incomingChar;
-// Bucle para leer todos los caracteres disponibles
-while (sim.available()) {
-incomingChar = sim.read(); // Leer carácter por carácter
-message += incomingChar; // Agregar carácter al mensaje
-delay(10); // Pequeña pausa para asegurar la lectura completa
-}
-message.trim(); // Construir el mensaje a partir de la lectura del puerto serial
-message.replace("\n","");
-message.replace("\r","");
-message.replace("\"", "'");
-Serial.println("Leido: " + message);
-int index = validarFormatoCMTI(message);
-leerMensaje(index);
-}
-}
+
 bool enviarMensaje(String message) {
-digitalWrite(ledPin, HIGH); // Encender LED para indicar que hay un mensaje que se va a enviar
-message.trim(); // Construir el mensaje a partir de la lectura del puerto serial
-message.replace("\n","");
-message.replace("\r","");
-message.replace("\"", "'");
-Serial.println("Mensaje enviado: " + message); // Imprimir mensaje en el puerto serie para verificación
-bool isTrue = sendPostRequest(message); // Enviar el mensaje por HTTP POST
-digitalWrite(ledPin, LOW); // Apagar el LED después de enviar el mensaje
-return isTrue;
+  digitalWrite(LED, HIGH); // Encender LED para indicar que hay un mensaje que se va a enviar
+  message = cleanString(message);
+  Serial.println("Mensaje enviado: " + message); // Imprimir mensaje en el puerto serie para verificación
+  bool isTrue = sendPostRequest(message); // Enviar el mensaje por HTTP POST
+  digitalWrite(LED, LOW); // Apagar el LED después de enviar el mensaje
+  return isTrue;
 }
+
 bool sendPostRequest(String message) {
-bool answer = false;
-if (WiFi.status() == WL_CONNECTED) {
-HTTPClient http;
-Serial.println("Enviando mensaje");
-// Iniciar la conexión con el objeto WiFiClient y la URL
-http.begin(client, url);
-http.addHeader("Content-Type", "application/json"); // Header de la petición
-String jsonData = "{\"datos\":\"" + message + "\"}";
-Serial.println("json: " + jsonData);
-int httpResponseCode = http.POST(jsonData);
-Serial.print("Codigo:");
-Serial.println(httpResponseCode);
-if (httpResponseCode > 0) {
-String response = http.getString(); // Obtener respuesta del servidor
-Serial.println("Respuesta del servidor: " + response);
-answer = true;
-} else {
-Serial.println("Error en la petición POST");
-Serial.println(http.getString());
+  bool answer = false;
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    Serial.println("Enviando mensaje");
+    // Iniciar la conexión con el objeto WiFiClient y la URL
+    http.begin(client, URL);
+    http.addHeader("Content-Type", "application/json"); // Header de la petición
+    String jsonData = "{\"datos\":\"" + message + "\"}";
+    Serial.println("json: " + jsonData);
+    int httpResponseCode = http.POST(jsonData);
+    Serial.print("Codigo:");
+    Serial.println(httpResponseCode);
+    if (httpResponseCode > 0) {
+      String response = http.getString(); // Obtener respuesta del servidor
+      Serial.println("Respuesta del servidor: " + response);
+      answer = true;
+    } else {
+      Serial.println("Error en la petición POST");
+      Serial.println(http.getString());
+    }
+    http.end(); // Terminar la conexión
+    delay(2000);
+  } else {
+    Serial.println("Error de conexión WiFi");
+  }
+  return answer;
 }
-http.end(); // Terminar la conexión
-delay(2000);
-} else {
-Serial.println("Error de conexión WiFi");
-}
-return answer;
-}
+
 void RecieveMessage() { 
   Serial.println("\n......................"); 
   Serial.println("Leyendo mensajes....."); 
-  sim.println("AT+CMGF=1"); 
+  SIM800L.println("AT+CMGF=1"); 
   delay(500); 
-  sim.println("AT+CNMI=1,2,0,0,0"); // AT Command to receive a live SMS 
+  SIM800L.println("AT+CNMI=1,2,0,0,0"); // AT Command to receive a live SMS 
   delay(500); 
   Serial.println("Unread Message done"); 
   Serial.println("......................\n"); 
+}
+
+String _readSerial() { 
+  Serial.println("Leyendo buffer"); 
+  _timeout = 0; 
+  while (!SIM800L.available() && _timeout < 12000) { 
+    if(SIM800L.available())Serial.println("Hy algo"); 
+    delay(13); 
+    _timeout++; 
+    Serial.println("."); 
   }
-  
-  String _readSerial() { 
-    Serial.println("Leyendo buffer"); 
-    _timeout = 0; 
-    while (!sim.available() && _timeout < 12000) { 
-      if(sim.available())Serial.println("Hy algo"); 
-      delay(13); 
-      _timeout++; 
-      Serial.println("."); 
-      }
-      if (sim.available()) { 
-        return sim.readString(); 
-        } 
-        else { 
-          return ""; // Agrega un valor de retorno predeterminado aquí 
-          } 
-          }
-          
-          void leerMensaje(const int &index) { 
-            Serial.println("Leyendo mensaje en la ubicación: " + String(index)); // Enviar comando AT para leer el mensaje 
-            sim.println("AT+CMGR=" + String(index)); 
-            delay(100); // Esperar respuesta del módulo 
-            String message = sim.readString(); // Mostrar la respuesta completa 
-            Serial.println("Respuesta hexadecimal del módulo:"); 
-            Serial.println(message); 
-            bool isTrue = enviarMensaje(message); 
-            if(isTrue){ borrar1Mensaje(index); 
-            } 
-            }
-            int validarFormatoCMTI(String message) { 
-              message.replace("+CMTI: 'SM',",""); 
-              Serial.println("Mensaje extraido: "+message); 
-              return message.toInt(); 
-              }
-              
-              void borrar1Mensaje(int index){ // Borra el mensaje en la posición 1 
-              Serial.println("Borrando mensaje: AT+CMGD="+index); 
-              sim.println("AT+CMGD="+index); // Borrar el primer mensaje 
-              delay(1000); // Esperar respuesta 
-              while (sim.available()) { 
-                String response = sim.readString(); 
-                Serial.println(response); // Imprimir la respuesta del módulo 
-                }
-                Serial.println("Mensaje borrado"); 
-                }void borrarTodosMensajes(){ 
-                  Serial.println("Borrando todos los mensajes"); // Para borrar todos los mensajes 
-                  sim.println("AT+CMGD=0"); // Borra todos los mensajes 
-                  delay(1000); // Esperar respuesta 
-                  while (sim.available()) { 
-                    String response = sim.readString(); 
-                    Serial.println(response); // Imprimir la respuesta del módulo 
-                    
-                    }Serial.println("Mensajes borrados"); 
-                    }
+  if (SIM800L.available()) { 
+    return SIM800L.readString(); 
+  } 
+  else { 
+    return ""; // Agrega un valor de retorno predeterminado aquí 
+  } 
+}
+
+void leerMensaje(const int &index) { 
+  Serial.println("Leyendo mensaje en la ubicación: " + String(index)); // Enviar comando AT para leer el mensaje 
+  SIM800L.println("AT+CMGR=" + String(index)); 
+  delay(100); // Esperar respuesta del módulo 
+  String message = SIM800L.readString(); // Mostrar la respuesta completa 
+  Serial.println("Respuesta hexadecimal del módulo:"); 
+  Serial.println(message); 
+  bool isTrue = enviarMensaje(message); 
+  if(isTrue){ borrar1Mensaje(index); 
+  } 
+}
+
+int validarFormatoCMTI(String message) { 
+  message.replace("+CMTI: 'SM',",""); 
+  Serial.println("Mensaje extraido: "+message); 
+  return message.toInt(); 
+}
+
+void borrar1Mensaje(int index){ // Borra el mensaje en la posición 1 
+  Serial.println("Borrando mensaje: AT+CMGD="+index); 
+  SIM800L.println("AT+CMGD="+index); // Borrar el primer mensaje 
+  delay(1000); // Esperar respuesta 
+  while (SIM800L.available()) { 
+    String response = SIM800L.readString(); 
+    Serial.println(response); // Imprimir la respuesta del módulo 
+  }
+  Serial.println("Mensaje borrado"); 
+}
+
+void borrarTodosMensajes(){ 
+  Serial.println("Borrando todos los mensajes"); // Para borrar todos los mensajes 
+  SIM800L.println("AT+CMGD=0"); // Borra todos los mensajes 
+  delay(1000); // Esperar respuesta 
+  while (SIM800L.available()) { 
+    String response = SIM800L.readString(); 
+    Serial.println(response); // Imprimir la respuesta del módulo 
+  }
+  Serial.println("Mensajes borrados"); 
+}
